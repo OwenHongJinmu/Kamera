f89e537849032c02a191442b0efb37383aa57ecc
OwenHong <bjl1@163.com>
2019-03-23 18:33:47 +0800

    A preview for video capture.

--- Kamera/PreviewView.swift
+++ Kamera/PreviewView.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+import UIKit
+import AVFoundation
+
+class PreviewContentView: UIView {
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func videoPreviewLayer() -> AVCaptureVideoPreviewLayer {
+        return self.layer as! AVCaptureVideoPreviewLayer
+    }
+}
+
+class PreviewView: UIView {
+    var captureSession: AVCaptureSession? {
+        didSet {
+            if let session = captureSession {
+                self.videoPreviewLayer().session = session
+            }
+        }
+    }
+    
+    var contentView: PreviewContentView = PreviewContentView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.frame = self.bounds
+        self.addSubview(contentView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func rectOfInterest() -> CGRect {
+        return CGRect.zero;
+    }
+    
+    func videoPreviewLayer() -> AVCaptureVideoPreviewLayer {
+        return self.contentView.layer as! AVCaptureVideoPreviewLayer
+    }
+}
+
+
